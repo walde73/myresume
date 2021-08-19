99083c0be0abe6cf450f4376aae22a2c91fa723b
walde73 <75583669+walde73@users.noreply.github.com>
2021-08-19 17:54:19 +0100

    Add files via upload

---
+++ zuri Displayname/Main.java
@@ -0,0 +1,20 @@
+package com.zuri;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String Firstname;
+        String Lastname;
+        String Middlename;
+
+        //Giving values to the String
+        Firstname ="Adebowale";
+        Lastname ="Adeyemo";
+        Middlename ="Samuel";
+
+        //Printing firstname, lastname and middlename.
+        System.out.println("My names is " + Firstname + " " + Lastname + " " + Middlename +".");
+    }
+
+}
+
